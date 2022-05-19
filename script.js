a9e68606c58a54bcc8252f1e89d5f840e442ee10
Cerina Malaluan <104408735+cerinahp@users.noreply.github.com>
2022-05-19 01:18:45 +0000

    working on js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Obtain the img element, and assign it to the image variable
-let image = document.getElementById("pet").src="https://placedog.net/800/640?id=15";
+let img = document.getElementById("#pet") 
 
 
 // Obtain the 'shrink-grow' button, and assign it to the changeSize variable
-let changeSize = document.getElementById("shrink/grow")
+let changeSize = document.getElementById("#shrink/grow")
 
 
 
